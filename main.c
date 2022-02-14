78d7d4312a84458a12cdbb1a8e12bcb90a817025
Isaid Castillo <85410710+IsaidCM-B@users.noreply.github.com>
2022-02-14 05:41:31 +0000

    Control versión codigo con float

--- main.c
+++ main.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
-
-int main(void) {
-  printf("Hello World\n");
+void bubble_sort(float a[],
+ int n) {
+    int i = 0, j = 0, tmp;
+    for (i = 0; i < n; i++) {   // bucle n veces - 1 por elemento
+        for (j = 0; j < n - i - 1; j++) { // los últimos i elementos ya están ordenados
+            if (a[j] > a[j + 1]) {  // cambiar si la orden se rompe
+                tmp = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = tmp;
+            }
+        }
+    }
+}
+int main() {
+  int a[100], n; 
+  float i; //d, swap;
+  printf("Ingrese el numero de elementos del arreglo:\n");
+  scanf("%d", &n); 
+  printf("Ingrese las fracciones:\n", n);
+  for (i = 0; i < n; i++)
+    scanf("%f", &a[i]);
+  bubble_sort("%f" a , n);
+  printf("Imprimiendo la matriz ordenada:\n");
+  for (i = 0; i < n; i++)
+     printf("%f\n", a[i]);
   return 0;
 }
+
+//Algoritmo de ordenamiento para numeros enteros donde tenemos dos bucles
