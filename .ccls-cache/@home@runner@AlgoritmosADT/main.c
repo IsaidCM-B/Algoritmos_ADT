5bc23275d01301d647e94cf6d86c7089e41749f4
Isaid Castillo <85410710+IsaidCM-B@users.noreply.github.com>
2022-02-14 05:45:14 +0000

    El que sirve con enteros y tiene la versión de fracciones

--- .ccls-cache/@home@runner@AlgoritmosADT/main.c
+++ .ccls-cache/@home@runner@AlgoritmosADT/main.c
@@ -28,3 +28,30 @@ int main() {
 }
 
 //Algoritmo de ordenamiento para numeros enteros donde tenemos dos bucles
+
+#include <stdio.h>
+void bubble_sort(int a[], int n) {
+    int i = 0, j = 0, tmp;
+    for (i = 0; i < n; i++) {   //bucle n veces - 1 por elemento
+        for (j = 0; j < n - i - 1; j++) { // los últimos i elementos ya están ordenados
+            if (a[j] > a[j + 1]) {  // cambiar si la orden se rompe
+                tmp = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = tmp;
+            }
+        }
+    }
+}
+int main() {
+  int a[100], n, i, d, swap;
+  printf("Ingrese el numero de elementos del arreglo:\n");
+  scanf("%d", &n); 
+  printf("Ingrese las fracciones:\n", n);
+  for (i = 0; i < n; i++)
+    scanf("%d", &a[i]);
+  bubble_sort(a, n);
+  printf("Imprimiendo la matriz ordenada:\n");
+  for (i = 0; i < n; i++)
+     printf("%d\n", a[i]);
+  return 0;
+}
